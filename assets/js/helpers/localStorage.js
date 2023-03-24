9387d46981e9e97340c7f41912432f4eb26d163c
uli <aldoulisesbc@gmail.com>
2023-03-23 18:27:03 -0600

    Mejora del codigo Main

---
+++ assets/js/helpers/localStorage.js
@@ -0,0 +1,59 @@
+function verifyTheme() {
+    const btn = document.getElementById('btn--theme')
+const body = document.body
+const footer = document.querySelector('.footer')
+const ls = window.localStorage
+ const main = document.querySelector('.main-main')
+const loade = document.querySelector('.loader') 
+const header =document.querySelector('.header')
+
+const cartCss = document.querySelector('.cart') 
+
+
+// comprabar si la clase esta en ls
+
+// si esta aplicamos al body esa clase
+
+// removemos del body esa clase
+
+const theme = ls.getItem('theme')
+
+if(theme === 'dark') {
+    header.classList.add('dark')
+  body.classList.add('dark')
+  footer.classList.add('dark')
+  loade.classList.add('dark')
+    main.classList.add('dark')
+    cartCss.classList.add('dark')
+
+} else { 
+    header.classList.remove('dark')
+  body.classList.remove('dark')
+  footer.classList.remove('dark')
+  loade.classList.remove('dark')
+    main.classList.remove('dark')
+    cartCss.classList.remove('dark')
+ 
+}
+
+btn.addEventListener('click', function () {
+ body.classList.toggle('dark')
+ footer.classList.toggle('dark')
+ loade.classList.toggle('dark')
+ main.classList.toggle('dark')
+ header.classList.toggle('dark')
+ cartCss.classList.toggle('dark')
+ // tenemos que detectar que el body tenga la clase dark
+ if (body.classList.contains('dark')) {
+   // si la tiene gurdamos esa clase en ls
+   ls.setItem('theme', 'dark')
+  } else {
+    // guardamos ese valor ls
+    ls.setItem('theme', 'light')
+ }
+})
+
+
+}
+
+export default verifyTheme
